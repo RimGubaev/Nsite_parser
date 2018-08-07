21c700fd8b849b6c71c4f0f5b91269deaf8fd06a
RimGubaev <rimgubaev@gmail.com>
2018-08-07 16:43:54 +0300

    Update Nsite_parser.py

--- Nsite_parser.py
+++ Nsite_parser.py
@@ -7,7 +7,7 @@ b = sys.argv[2] # reading of output filename (second argument)
 
 ouf = open(b, 'w+') # open output file for writing
 
-ouf.write('GeneID\tLenght\tFactorID\tStrand\tMean_exp_value\tFound\n') # set the columns names
+ouf.write('GeneID\tLength\tFactorID\tStrand\tMean_exp_value\tFound\n') # set the columns names
 
 with open(a) as inf:
     for line in inf:
@@ -19,7 +19,7 @@ with open(a) as inf:
             FactorID = line[9:17] # saving TF binding site ID name into FactorID variable untill next TF binding site
             #print(FactorID)
         if line[0:25] == 'Length of Query Sequence:':
-            Lenght = re.search('\d+\sbp', line).group(0) # saving the promoter sequence length (по регулярному выражению)
+            Length = re.search('\d+\sbp', line).group(0) # saving the promoter sequence length (by regex)
         if 'Motifs' in line:
             Strand = line[line.find('" Strand:')-1] # saving TF binding site ID name into FactorID variable until next TF binding site
             #print(Strand)
@@ -28,6 +28,6 @@ with open(a) as inf:
             Found = re.search('Found\s*\d+', line) 
             Found = re.search('\d+', Found.group(0)).group(0) # saving the number of TF binding sites
             #print(Found)
-            ouf.write(GeneID + '\t' + Lenght + '\t' + FactorID + '\t' 
+            ouf.write(GeneID + '\t' + Length + '\t' + FactorID + '\t' 
                       + Strand + '\t' + Mean_exp_value + '\t' + Found +'\n') # adding a string in tab-separated format          
 ouf.close()
